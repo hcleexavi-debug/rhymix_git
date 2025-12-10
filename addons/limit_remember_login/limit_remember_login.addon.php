d7a6bc199afe9dd199c02adcc227cd78c4d36fa9
hclee-macmini <hcleexavi@gmail.com>
2025-12-10 22:51:22 +0900

    로그인 유지,  리스트 css

--- addons/limit_remember_login/limit_remember_login.addon.php
+++ addons/limit_remember_login/limit_remember_login.addon.php
@@ -1,5 +1,6 @@
 <?php
 // error_log('limit_remember_login.addon.php called : ' . __LINE__);
+
 // if (!defined('RX_VERSION')) exit;
 if(!defined("__XE__")) exit();
 // 로그인 유지를 허용할 IP 목록
@@ -8,7 +9,7 @@ $allowed_ips = array(
     '192.168.219.1',
     '59.7.27.131',
     '220.127.198.145',
-    '121.136.188.196',
+    '121.136.188.196', // 2025-12-10, hclee, 박정옥
     // 필요한 IP를 여기에 추가
 );
 // 현재 접속자의 IP
@@ -17,7 +18,7 @@ $user_ip = RX_CLIENT_IP;
 // 로그인 처리 시 (실제 로그인 실행 전)
 if ($called_position == 'before_module_proc') {
     $act = Context::get('act');
-    if ($act == 'procMemberLogin') {
+    if ($act == 'procMemberLogin' || $act == '') { // 2026-12-10, hclee, 메인페이지도
         // 허용되지 않은 IP라면 로그인 유지 강제 해제
         if (!in_array($user_ip, $allowed_ips)) {
             Context::set('keep_signed', 'N');
@@ -28,7 +29,7 @@ if ($called_position == 'before_module_proc') {
 // 로그인 폼 화면에서 (체크박스 숨김)
 if ($called_position == 'before_display_content') {
     $act = Context::get('act');
-    if ($act == 'dispMemberLoginForm') {
+    if ($act == 'dispMemberLoginForm' || $act == '') { // 2026-12-10, hclee, 메인페이지도
         // error_log("limit_remember_login.addon.php called : [{$act}]" . __LINE__);
         // 허용되지 않은 IP라면 로그인 유지 체크박스 숨김
         if (!in_array($user_ip, $allowed_ips)) {
